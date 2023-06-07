036b357536dcfac75a532dc773142647172e9b4c
harish242 <markollh242@gmail.com>
2023-06-07 13:10:38 +0530

    jhi

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,40 @@
-import React,{useState} from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { PhotoFrame } from './PhotoFrame';
+import React, { useState } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { PhotoFrame } from "./PhotoFrame";
 const App = () => {
-    const [id,setNumber]=useState()
-    const[data,setData]=useState()
-    const[isLoading,setIsLoading]=useState(false)
-   
-    const HandleChange=async (event)=>{
-       setNumber(event.target.value)
-       setIsLoading(true)
-    const response=await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`)
-    const data=await response.json()
-    console.log(data)
-    setData(data)
-    setIsLoading(false)
+  const [id, setNumber] = useState();
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
-    }
-   
-  return(
+  const HandleChange = async (event) => {
+    const number = event.target.value;
+    setNumber(number);
+
+    setTimeout(async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/photos/${number}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+      setIsLoading(false);
+    }, 0);
+  };
+
+  return (
     <>
-    <input type="number" placeholder="number" onChange={HandleChange} value={id}/>
-    {isLoading&&<Loader/>}
-    {data&&<PhotoFrame url={data.url} title={data.title}/>}
-    </> 
-    
-  )
-}
-
+      <input
+        type="number"
+        placeholder="number"
+        onChange={HandleChange}
+        value={id}
+      />
+      {isLoading && <Loader />}
+      {data && <PhotoFrame url={data.url} title={data.title} />}
+    </>
+  );
+};
 
 export default App;
